735341b4a2886e3f902b4880919f29989eecfbe0
StepanetsRoman <vtk241_srs@student.ztu.edu.ua>
2025-05-17 14:37:19 +0300

    Delete Lab3 directory

--- Lab3/Lab6/task1/delete_user.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-$data = json_decode(file_get_contents("php://input"));
-$conn = new mysqli("localhost", "root", "", "lab6");
-$id = (int)$data->id;
-$conn->query("DELETE FROM users WHERE id=$id");
-echo "Користувача видалено.";
-?>
